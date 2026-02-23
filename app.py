2f5b00dbff154c276f09275b8c67393375c14a70
LuutuGregoryValentino <luutugregory@gmail.com>
2026-02-23 17:13:56 +0300

    commented out transport_insecure

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ scheduler.add_job(
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #
+# os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #comment out for production environment, uncomment for running on local server
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 
 google_blueprint = make_google_blueprint(
